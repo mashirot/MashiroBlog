f3f4c61a05f915cb996e8545815cb079576900c8
mashirot <mashirotj@gmail.com>
2023-08-04 01:00:59 +0800

    打印异常Message

--- src/main/java/ski/mashiro/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/ski/mashiro/exception/handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package ski.mashiro.exception.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ski.mashiro.common.Result;
@@ -12,15 +13,18 @@ import static ski.mashiro.constant.StatusConstant.SYS_ERR;
  * @author MashiroT
  */
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public Result<String> exceptionHandler(Exception e) {
+        log.error("{}", e.getMessage());
         return Result.failed(SYS_ERR, "系统错误");
     }
 
     @ExceptionHandler(SecurityException.class)
     public Result<String> securityExceptionHandler(SecurityException e) {
+        log.error("{}", e.getMessage());
         return Result.failed(ACCESS_DENIED, e.getMessage());
     }
 
