1ca95a0e670a2424b5a10b8c2b94cebc781eb188
mashirot <mashirotj@gmail.com>
2023-08-20 04:24:50 +0800

    使用FreeMaker渲染html，作为邮件内容返回

--- mail-service/build.gradle.kts
+++ mail-service/build.gradle.kts
@@ -21,6 +21,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-amqp")
     implementation("org.springframework.boot:spring-boot-starter-mail")
+    implementation("org.springframework.boot:spring-boot-starter-freemarker")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.amqp:spring-rabbit-test")
--- mail-service/src/main/kotlin/ski/mashiro/service/impl/MailServiceImpl.kt
+++ mail-service/src/main/kotlin/ski/mashiro/service/impl/MailServiceImpl.kt
@@ -1,10 +1,12 @@
 package ski.mashiro.service.impl
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import freemarker.template.Configuration
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.mail.MailSender
-import org.springframework.mail.SimpleMailMessage
+import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.mail.javamail.MimeMessageHelper
 import org.springframework.stereotype.Service
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils
 import ski.mashiro.dto.CommentMailDTO
 import ski.mashiro.service.MailService
 import java.util.*
@@ -15,27 +17,44 @@ import java.util.*
 @Service
 class MailServiceImpl(
     val objectMapper: ObjectMapper,
-    val mailSender: MailSender
+    val mailSender: JavaMailSender,
+    val freeMakerConfiguration: Configuration
 ) : MailService {
     @Value("\${blog.from}")
     private var from: String? = null
+
     @Value("\${blog.to}")
     private var to: String? = null
+
     override fun sendNewCommentAdvice2Owner(msg: String) {
         try {
             val commentMailDTO = objectMapper.readValue(msg, CommentMailDTO::class.java)
-            val simpleMailMessage = SimpleMailMessage()
-            simpleMailMessage.from = from
-            simpleMailMessage.setTo(to)
-            simpleMailMessage.subject = "你的帖子「${commentMailDTO.articleTitle}」有新回复"
-            simpleMailMessage.text = """
-                @${commentMailDTO.senderNickname} 回复了你：
-                ${if (commentMailDTO.content!!.length > 50) commentMailDTO.content.substring(0, 50) else commentMailDTO.content}
-                """.trimIndent()
-            simpleMailMessage.sentDate = Date()
-            mailSender.send(simpleMailMessage)
+
+            val mimeMessage = mailSender.createMimeMessage()
+            val mimeMessageHelper = MimeMessageHelper(mimeMessage, "utf-8")
+            mimeMessageHelper.setFrom(from!!)
+            mimeMessageHelper.setTo(to!!)
+            val title = "你的帖子「${commentMailDTO.articleTitle}」有新回复"
+            mimeMessageHelper.setSubject(title)
+
+            val map = mapOf(
+                "title" to title,
+                "senderNickname" to commentMailDTO.senderNickname!!,
+                "content" to if (commentMailDTO.content!!.length > 50) commentMailDTO.content.substring(
+                    0,
+                    50
+                ) else commentMailDTO.content
+            )
+            val template = freeMakerConfiguration.getTemplate("CommentAdvice.ftlh")
+            val text = FreeMarkerTemplateUtils.processTemplateIntoString(template, map)
+
+            mimeMessageHelper.setText(text, true)
+            mimeMessageHelper.setSentDate(Date())
+
+            mailSender.send(mimeMessage)
         } catch (e: Exception) {
             e.printStackTrace()
         }
     }
+
 }
